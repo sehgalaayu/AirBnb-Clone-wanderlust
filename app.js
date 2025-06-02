586af1498d63ca1d285343d3ba67eb46b6b4dbca
sehgalaayu <119155282+sehgalaayu@users.noreply.github.com>
2025-06-02 15:31:43 +0530

    Update

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "public")));
 
 main()
   .then(() => console.log("Connection to MongoDB Successful!"))
